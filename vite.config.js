4380ba7fd907ebcc32332165bf5e4020916fc0d8
MohdTamim <shaikhtamim2002@gmail.com>
2024-08-27 23:46:49 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "jsm-x9",
@@ -10,6 +9,7 @@ export default defineConfig({
   })],
 
   build: {
-    sourcemap: true
+    sourcemap: true,
+    chunkSizeWarningLimit: 1600, // Increase the warning limit to 1000 kB
   }
 })
